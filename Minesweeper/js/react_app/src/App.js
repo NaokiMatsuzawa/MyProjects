c744cbde2cd60f8b0197bc749dbb78fa001c9041
NaokiMatsuzawa <naokim1004@gmail.com>
2023-03-09 14:02:21 +0900

    delete not needed files, make raugh table.

--- Minesweeper/js/react_app/src/App.js
+++ Minesweeper/js/react_app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+
+function Square(){
+  return(
+    <div className="Square">
+        X
+    </div>
+  );
+}
+
+function Field(){
+  const xArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const yArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return(
+    <table>
+      {yArray.map(_ => <tr>{xArray.map(_ => <td><Square /></td>)}</tr> )}
+    </table>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Field />
     </div>
   );
 }
